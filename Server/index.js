b02dca96e52ae534755e8dc75caae8cc5ea7cf0f
Manendra Verma <manuvma03@gmail.com>
2024-04-13 23:28:05 +0530

    fix : sockets Link for deployment

--- Server/index.js
+++ Server/index.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === 'development') {
 
 const io = socket(server, {
     cors: {
-        origin: 'https://chat-app-scaler.vercel.app/',
+        origin: 'https://chat-app-scaler.vercel.app',
         credentials: true
     }
 });
